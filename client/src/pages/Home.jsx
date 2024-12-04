379bce5d3aef87de08150ce288dd6455fbe6b691
Siddhartha Pal <siddhartha.pal.official@gmail.com>
2024-12-04 13:13:10 +0530

    Update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/Steps'
+import Bgslider from '../components/Bgslider'
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Steps />
+      <Bgslider />
     </div>
   )
 }
