6b6e51d24803a4faf4d8704fab4c8a9cb94357e5
Siddhartha Pal <siddhartha.pal.official@gmail.com>
2024-12-04 22:57:37 +0530

    Update-2

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/Steps'
 import Bgslider from '../components/Bgslider'
+import Testimonials from '../components/Testimonials'
+import Upload from '../components/Upload'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Header />
       <Steps />
       <Bgslider />
+      <Testimonials />
+      <Upload />
     </div>
   )
 }
