cde5cfffaa6c96fb7f39b5b6481aacc3a5a67303
Siddhartha Pal <siddhartha.pal.official@gmail.com>
2024-12-07 13:16:47 +0530

    Update

--- server/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { clerkWebhooks } from '../controllers/UserController.js'
-
-const userRouter = express.Router()
-
-userRouter.post('/webhooks', clerkWebhooks)
-
-export default userRouter
