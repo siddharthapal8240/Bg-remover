605eb1fb264e740804278107c3519d51fd758fa0
Siddhartha Pal <siddhartha.pal.official@gmail.com>
2025-02-17 00:56:46 +0530

    Update

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
-// import userRouter from './routes/userRoutes.js'
+import userRouter from './routes/userRoutes.js'
 
 
 //API config
@@ -19,6 +19,6 @@ app.use(cors())
 //API routes
 
 app.get('/', (req, res) =>res.send('API working'))
-// app.use('/api/user',userRouter)
+app.use('/api/user',userRouter)
 
 app.listen(PORT, () => console.log("Server is running on port " + PORT))
